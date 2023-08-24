a4c86ddc2c746275df38c4335dbaca1b0573917b
Matroskin04 <zetwyn@mail.ru>
2023-08-24 11:13:05 +0900

    for vercel

--- 5_sprint_Mongoose_SQL_Nest/src/main.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 import { appSettings } from './app.settings';
+import pg from 'pg';
+
+const { Pool } = pg;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,5 +12,8 @@ async function bootstrap() {
   appSettings(app);
 
   await app.listen(process.env.PORT || 5000);
+  const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+  });
 }
 bootstrap();
