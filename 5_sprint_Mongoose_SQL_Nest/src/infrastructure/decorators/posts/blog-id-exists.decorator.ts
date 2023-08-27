b1a7431786489d52e7695f84ec91c732f999fa0b
Matroskin04 <zetwyn@mail.ru>
2023-08-27 12:28:31 +0900

    fixed some errors: 1) transported to SQL - constraint (checking blog id exist)

--- 5_sprint_Mongoose_SQL_Nest/src/infrastructure/decorators/posts/blog-id-exists.decorator.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/infrastructure/decorators/posts/blog-id-exists.decorator.ts
@@ -6,19 +6,20 @@ import {
 import { ObjectId } from 'mongodb';
 import { BlogsSARepository } from '../../../features/blogs/super-admin-blogs/infrastructure/repository/blogs-sa.repository';
 import { Injectable } from '@nestjs/common';
+import { BlogsPublicQueryRepository } from '../../../features/blogs/public-blogs/infrastructure/query.repository/blogs-public.query.repository';
 
 @ValidatorConstraint({ name: 'IsBlogByIdExists', async: true })
 @Injectable()
 export class IsBlogByIdExistsConstraint
   implements ValidatorConstraintInterface
 {
-  constructor(protected blogsSARepository: BlogsSARepository) {}
+  constructor(
+    protected blogsPublicQueryRepository: BlogsPublicQueryRepository,
+  ) {}
   async validate(value: string, args: ValidationArguments | any) {
     const blogId = args.object.blogId;
 
-    const blog = await this.blogsSARepository.getBlogInstance(
-      new ObjectId(blogId),
-    );
+    const blog = await this.blogsPublicQueryRepository.getBlogById(blogId);
     return !!blog;
   }
 
