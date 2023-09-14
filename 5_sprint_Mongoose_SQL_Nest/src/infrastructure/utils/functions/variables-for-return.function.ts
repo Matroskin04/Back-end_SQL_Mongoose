f2c69eeb500b3075dfff4cd383d22656eec992dc
Matroskin04 <zetwyn@mail.ru>
2023-09-14 15:08:05 +0700

    added logic to fix search by login or email

--- 5_sprint_Mongoose_SQL_Nest/src/infrastructure/utils/functions/variables-for-return.function.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/infrastructure/utils/functions/variables-for-return.function.ts
@@ -33,9 +33,17 @@ export function variablesForReturn(
     | 'ASC'
     | 'DESC';
 
-  const searchLoginTerm: string = query?.searchLoginTerm ?? '';
-  const searchEmailTerm: string = query?.searchEmailTerm ?? '';
+  let searchLoginTerm: string = query?.searchLoginTerm ?? '';
+  let searchEmailTerm: string = query?.searchEmailTerm ?? '';
   const searchNameTerm: string = query?.searchNameTerm ?? '';
+
+  //set value (if only 1 parameter was passed) - to fix OR in search
+  if (searchLoginTerm === '' && searchEmailTerm !== '') {
+    searchLoginTerm = 'upv298h92bD*f903-ud2hdn1~0r-~fuu0';
+  }
+  if (searchLoginTerm !== '' && searchEmailTerm === '') {
+    searchEmailTerm = 'upv298h92bD*f903-ud2hdn1~0r-~fuu0';
+  }
 
   let banStatus;
   if (!query?.banStatus || query?.banStatus === 'all') banStatus = null;
