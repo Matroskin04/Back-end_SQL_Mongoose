0f4bab4fe50f3872715e506dd073a35df7b259eb
Matroskin04 <zetwyn@mail.ru>
2023-09-17 21:18:12 +0700

    transported on typeORM delete device by id

--- 5_sprint_Mongoose_SQL_Nest/src/features/devices/application/use-cases/delete-device-by-id.use-case.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/features/devices/application/use-cases/delete-device-by-id.use-case.ts
@@ -1,8 +1,10 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { ResponseTypeService } from '../../../../infrastructure/utils/functions/types/create-responses-service.types.service';
-import { createResponseService } from '../../../../infrastructure/utils/functions/create-response-service.function';
+import { createResponseByCodeAndMessage } from '../../../../infrastructure/utils/functions/create-response-service.function';
 import { DevicesQueryRepository } from '../../infrastructure/SQL/query.repository/devices.query.repository';
 import { DevicesRepository } from '../../infrastructure/SQL/repository/devices.repository';
+import { DevicesOrmQueryRepository } from '../../infrastructure/typeORM/query.repository/devices-orm.query.repository';
+import { DevicesOrmRepository } from '../../infrastructure/typeORM/repository/devices-orm.repository';
 
 export class DeleteDeviceByIdCommand {
   constructor(public deviceId: string, public userId: string) {}
@@ -13,24 +15,28 @@ export class DeleteDeviceByIdUseCase
   implements ICommandHandler<DeleteDeviceByIdCommand>
 {
   constructor(
-    protected devicesQueryRepository: DevicesQueryRepository,
-    protected deviceRepository: DevicesRepository,
+    protected devicesOrmQueryRepository: DevicesOrmQueryRepository,
+    protected deviceOrmRepository: DevicesOrmRepository,
   ) {}
   async execute(
     command: DeleteDeviceByIdCommand,
   ): Promise<ResponseTypeService> {
     const { deviceId, userId } = command;
-    const device = await this.devicesQueryRepository.getDeviceById(deviceId);
+    const device = await this.devicesOrmQueryRepository.getDeviceById(deviceId);
 
-    if (!device) return createResponseService(404, 'The device is not found');
+    if (!device)
+      return createResponseByCodeAndMessage(404, 'The device is not found');
     if (device.userId !== userId)
-      return createResponseService(403, "You can't delete not your own device");
+      return createResponseByCodeAndMessage(
+        403,
+        "You can't delete not your own device",
+      );
 
-    const result = await this.deviceRepository.deleteDeviceById(deviceId);
+    const result = await this.deviceOrmRepository.deleteDeviceById(deviceId);
     if (!result) {
       throw new Error('The device is not found');
     }
 
-    return createResponseService(204, 'Successfully deleted');
+    return createResponseByCodeAndMessage(204, 'Successfully deleted');
   }
 }
