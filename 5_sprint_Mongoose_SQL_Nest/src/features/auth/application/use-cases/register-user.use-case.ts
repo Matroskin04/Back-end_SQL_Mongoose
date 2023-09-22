ea68d5bc59bf286a68c4b456ff3bf94b11f6870b
Matroskin04 <zetwyn@mail.ru>
2023-09-22 21:57:55 +0700

    some fixed

--- 5_sprint_Mongoose_SQL_Nest/src/features/auth/application/use-cases/register-user.use-case.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/features/auth/application/use-cases/register-user.use-case.ts
@@ -2,14 +2,12 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { v4 as uuidv4 } from 'uuid';
 import { CryptoAdapter } from '../../../../infrastructure/adapters/crypto.adapter';
 import { EmailManager } from '../../../../infrastructure/managers/email-manager';
-import { EmailConfirmationPublicRepository } from '../../../users/infrastructure/SQL/subrepository/email-confirmation.public.repository';
-import { PasswordRecoveryPublicRepository } from '../../../users/infrastructure/SQL/subrepository/password-recovery.public.repository';
-import { BanInfoPublicRepository } from '../../../users/infrastructure/SQL/subrepository/ban-info.public.repository';
-import { UsersRepository } from '../../../users/infrastructure/SQL/repository/users.repository';
 import { UsersOrmRepository } from '../../../users/infrastructure/typeORM/repository/users-orm.repository';
 import { EmailConfirmationOrmRepository } from '../../../users/infrastructure/typeORM/subrepository/email-confirmation-orm.public.repository';
 import { PasswordRecoveryOrmRepository } from '../../../users/infrastructure/typeORM/subrepository/password-recovery-orm.public.repository';
 import { BanInfoOrmRepository } from '../../../users/infrastructure/typeORM/subrepository/ban-info-orm.public.repository';
+import { DataSource } from 'typeorm';
+import dataSource from '../../../../db/data-source';
 
 export class RegisterUserCommand {
   constructor(
@@ -30,36 +28,51 @@ export class RegisterUserUseCase
     protected emailConfirmationPublicRepository: EmailConfirmationOrmRepository,
     protected passwordRecoveryPublicRepository: PasswordRecoveryOrmRepository,
     protected banInfoPublicRepository: BanInfoOrmRepository,
+    protected dataSource: DataSource,
   ) {}
 
   async execute(command: RegisterUserCommand): Promise<void> {
     const { email, login, password } = command;
+    const confirmationCode = uuidv4();
 
     const passwordHash = await this.cryptoAdapter._generateHash(password);
 
     const userId = uuidv4();
-    await this.usersOrmRepository.createUser(
-      userId,
-      login,
-      email,
-      passwordHash,
-    );
-    const confirmationCode = uuidv4();
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
 
-    await this.emailConfirmationPublicRepository.createEmailConfirmationInfo(
-      confirmationCode,
-      '5 hours',
-      false,
-      userId,
-    );
+    await queryRunner.startTransaction();
+    try {
+      await this.usersOrmRepository.createUser(
+        userId,
+        login,
+        email,
+        passwordHash,
+      );
 
-    await this.passwordRecoveryPublicRepository.createPassRecoveryInfo(
-      uuidv4(),
-      userId,
-    );
-    await this.banInfoPublicRepository.createBanInfoUser(userId);
+      await this.emailConfirmationPublicRepository.createEmailConfirmationInfo(
+        confirmationCode,
+        '5 hours',
+        false,
+        userId,
+      );
 
-    this.emailManager.sendEmailConfirmationMessage(email, confirmationCode);
+      await this.passwordRecoveryPublicRepository.createPassRecoveryInfo(
+        uuidv4(),
+        userId,
+      );
+      throw new Error();
+      await this.banInfoPublicRepository.createBanInfoUser(userId);
+
+      // commit transaction now:
+      await queryRunner.commitTransaction();
+      this.emailManager.sendEmailConfirmationMessage(email, confirmationCode);
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // you need to release query runner which is manually created:
+      await queryRunner.release();
+    }
     return;
   }
 }
