cd1fa02a3ab7f616ecc1ec3ddeb02543221a26fb
Matroskin04 <zetwyn@mail.ru>
2023-09-28 11:08:47 +0700

    added confition that some fields were validated if they are passed

--- 5_sprint_Mongoose_SQL_Nest/src/features/quiz/api/models/input/query-questions.input.model.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/features/quiz/api/models/input/query-questions.input.model.ts
@@ -14,11 +14,13 @@ const valuesOfSortingQuestions = [
 export class QueryQuestionsInputModel extends QueryGeneralParams {
   @ValidateIf((o) => o.sortBy !== undefined)
   @IsIn(valuesOfSortingQuestions)
+  @ValidateIf((o) => o.sortDirection !== undefined)
   sortBy?: string;
 
   @IsIn(['all', 'published', 'notPublished'], {
     message: 'The value should be one of these: all, published, notPublished',
   })
+  @ValidateIf((o) => o.sortDirection !== undefined)
   publishedStatus?: PublishedStatusType;
 
   bodySearchTerm?: string;
