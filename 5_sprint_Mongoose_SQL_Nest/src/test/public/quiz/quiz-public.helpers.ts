db21f87af89a468404c2d92e8dbb9abbfdbfebc6
Matroskin04 <zetwyn@mail.ru>
2023-10-02 10:42:54 +0700

    created Helper for tests: create response quiz

--- 5_sprint_Mongoose_SQL_Nest/src/test/public/quiz/quiz-public.helpers.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/test/public/quiz/quiz-public.helpers.ts
@@ -1,7 +1,45 @@
 import request from 'supertest';
+import { QuizStatusType } from '../../../infrastructure/types/quiz-questions.general.types';
 
 export async function connectPlayerToQuiz(httpServer, accessToken?) {
   return request(httpServer)
     .post(`/hometask-nest/pair-game-quiz/pairs/connection`)
     .set('Authorization', `Bearer ${accessToken}`);
+}
+
+export function createResponseSingleQuizTest(
+  quizStatus?: QuizStatusType,
+  startDate?: null | 'string',
+  quizId?: string | null,
+  user1Id?: string | null,
+  score1?: number | null,
+  user2Id?: string | null,
+  login2?: string | null,
+  score2?: number | null,
+  finishDate?: null | 'string',
+) {
+  return {
+    id: quizId ?? expect.any(String),
+    firstPlayerProgress: {
+      answers: [],
+      player: {
+        id: user1Id ?? expect.any(String),
+        login: expect.any(String),
+      },
+      score: score1 ?? expect.any(Number),
+    },
+    secondPlayerProgress: {
+      answers: [],
+      player: {
+        id: user2Id ? expect.any(String) : null,
+        login: login2 ? expect.any(String) : null,
+      },
+      score: score2 ?? expect.any(Number),
+    },
+    questions: [],
+    status: quizStatus ?? expect.any(String),
+    pairCreatedDate: expect.any(String),
+    startGameDate: typeof startDate === 'string' ? expect.any(String) : null,
+    finishGameDate: typeof finishDate === 'string' ? expect.any(String) : null,
+  };
 }
