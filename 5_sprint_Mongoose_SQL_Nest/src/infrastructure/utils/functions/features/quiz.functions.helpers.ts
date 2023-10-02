954341a562d704431a2359b27a6af4a705b90717
Matroskin04 <zetwyn@mail.ru>
2023-10-02 09:15:31 +0700

    created the helper: for modifying quiz into view model

--- 5_sprint_Mongoose_SQL_Nest/src/infrastructure/utils/functions/features/quiz.functions.helpers.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/infrastructure/utils/functions/features/quiz.functions.helpers.ts
@@ -1,4 +1,7 @@
 import { QuestionQuizAllInfoType } from '../../../../features/quiz/infrastructure/typeORM/repository/questions/questions.types.repository';
+import { QuizViewType } from '../../../../features/quiz/infrastructure/typeORM/query.repository/quiz/quiz.types.query.repository';
+import { QuizStatusEnum } from '../../enums/quiz.enums';
+import { QuizStatusType } from '../../../types/quiz-questions.general.types';
 
 export function modifyQuestionIntoViewModel(question): QuestionQuizAllInfoType {
   return {
@@ -12,3 +15,50 @@ export function modifyQuestionIntoViewModel(question): QuestionQuizAllInfoType {
     updatedAt: question.updatedAt ? question.updatedAt.toString() : null,
   };
 }
+
+export function modifyQuizIntoViewModel(
+  quizInfo: QuizAllInfoRawType,
+): QuizViewType {
+  return {
+    id: quizInfo.id,
+    firstPlayerProgress: {
+      answers: quizInfo.answers1 ?? [],
+      player: {
+        id: quizInfo.user1Id,
+        login: quizInfo.login1,
+      },
+      score: quizInfo.score1,
+    },
+    secondPlayerProgress: {
+      answers: quizInfo.answers2 ?? [],
+      player: {
+        id: quizInfo.user2Id,
+        login: quizInfo.login2,
+      },
+      score: quizInfo.score2,
+    },
+    questions: quizInfo.questions ?? [],
+    status: QuizStatusEnum[quizInfo.status],
+    pairCreatedDate: quizInfo.pairCreatedDate.toString(),
+    startGameDate: quizInfo.startGameDate.toString(),
+    finishGameDate: quizInfo.finishGameDate.toString(),
+  };
+}
+
+export type QuizAllInfoRawType = {
+  id: string;
+  user1Id: string;
+  login1: string;
+  score1: number;
+  user2Id: string;
+  login2: string;
+  score2: number;
+  secondPlayerProgress: [];
+  questions: [];
+  answers1: [];
+  answers2: [];
+  status: QuizStatusType;
+  pairCreatedDate: Date;
+  startGameDate: Date;
+  finishGameDate: Date;
+};
