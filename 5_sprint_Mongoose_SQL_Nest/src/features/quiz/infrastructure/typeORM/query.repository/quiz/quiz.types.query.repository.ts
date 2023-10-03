d668d125166b373904f472e9874079d889400fc1
Matroskin04 <zetwyn@mail.ru>
2023-10-03 15:12:46 +0700

    fixed type status of quiz

--- 5_sprint_Mongoose_SQL_Nest/src/features/quiz/infrastructure/typeORM/query.repository/quiz/quiz.types.query.repository.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/features/quiz/infrastructure/typeORM/query.repository/quiz/quiz.types.query.repository.ts
@@ -2,6 +2,7 @@ import {
   QuizAnswerStatusEnum,
   QuizStatusEnum,
 } from '../../../../../../infrastructure/utils/enums/quiz.enums';
+import { QuizStatusType } from '../../../../../../infrastructure/types/quiz-questions.general.types';
 
 export type UsersIdsOfQuizType = { user1Id: string; user2Id: string | null };
 
@@ -10,7 +11,7 @@ export type QuizViewType = {
   firstPlayerProgress: InfoAboutUserQuizType;
   secondPlayerProgress: InfoAboutUserQuizType | null;
   questions: QuestionOfQuizType[] | null;
-  status: QuizStatusEnum;
+  status: QuizStatusType;
   pairCreatedDate: string;
   startGameDate: string | null;
   finishGameDate: string | null;
