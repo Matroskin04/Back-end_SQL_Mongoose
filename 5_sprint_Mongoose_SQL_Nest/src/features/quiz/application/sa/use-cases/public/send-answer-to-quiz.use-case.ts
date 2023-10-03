35244c4e8b713885579438a2b503088a17163f45
Matroskin04 <zetwyn@mail.ru>
2023-10-03 15:22:25 +0700

    creating logic for sending answers to quiz

--- 5_sprint_Mongoose_SQL_Nest/src/features/quiz/application/sa/use-cases/public/send-answer-to-quiz.use-case.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/features/quiz/application/sa/use-cases/public/send-answer-to-quiz.use-case.ts
@@ -5,6 +5,9 @@ import { QuestionQuizRelationOrmRepository } from '../../../../infrastructure/ty
 import { QuizOrmRepository } from '../../../../infrastructure/typeORM/repository/quiz/quiz-orm.repository';
 import { QuizInfoAboutUserOrmRepository } from '../../../../infrastructure/typeORM/repository/quiz-info-about-user-orm.repository';
 import { DataSource } from 'typeorm';
+import { ForbiddenException } from '@nestjs/common';
+import { QuizInfoAboutUserQueryRepository } from '../../../../infrastructure/typeORM/query.repository/quiz-info-about-user/quiz-info-about-user.query.repository';
+import { QuizInfoAboutUser } from '../../../../domain/quiz-game-info-about-user.entity';
 
 export class SendAnswerToQuizCommand {
   constructor(public userId: string, public answer: string) {}
@@ -16,14 +19,46 @@ export class ConnectToQuizUseCase
 {
   constructor(
     protected quizOrmQueryRepository: QuizOrmQueryRepository,
+    protected quizInfoAboutUserQueryRepository: QuizInfoAboutUserQueryRepository,
     protected questionsOrmQueryRepository: QuestionsOrmQueryRepository,
-    protected questionQuizRelationOrmRepository: QuestionQuizRelationOrmRepository,
-    protected quizOrmRepository: QuizOrmRepository,
-    protected quizInfoAboutUserOrmRepository: QuizInfoAboutUserOrmRepository,
     protected dataSource: DataSource,
   ) {}
 
   async execute(command: SendAnswerToQuizCommand): Promise<any> {
     const { userId, answer } = command;
+
+    const activeQuiz = await this.quizOrmQueryRepository.getCurrentQuizByUserId(
+      userId,
+    );
+    //check that user has an active game
+    if (!activeQuiz)
+      throw new ForbiddenException('Active quiz game is not found');
+
+    if (!activeQuiz.questions || !(activeQuiz.status === 'Active'))
+      throw new Error(
+        "Questions are not found or status of quiz is not 'Active'",
+      );
+
+    //check a number of user answers
+    const answersNumber =
+      await this.quizInfoAboutUserQueryRepository.getNumberOfAnswersById(
+        activeQuiz.id,
+        userId,
+      );
+    if (!answersNumber) throw new Error('Number of answers is not found');
+
+    if (answersNumber === 5)
+      throw new ForbiddenException(
+        'All the answers have already been received',
+      );
+
+    const currentQuestionId = activeQuiz.questions[answersNumber].id;
+    const correctAnswers =
+      await this.questionsOrmQueryRepository.getAnswersOfQuestion(
+        currentQuestionId,
+      );
+    //if it is the last answer:
+    if (answersNumber === 4) {
+    }
   }
 }
