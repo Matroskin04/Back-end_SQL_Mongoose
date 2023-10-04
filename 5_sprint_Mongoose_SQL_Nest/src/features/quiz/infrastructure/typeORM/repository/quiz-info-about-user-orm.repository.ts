dad97688c3d31aef0c222e3a02a15cc3f8b80ab2
Matroskin04 <zetwyn@mail.ru>
2023-10-04 14:04:22 +0700

    fixed query of incrementing the score

--- 5_sprint_Mongoose_SQL_Nest/src/features/quiz/infrastructure/typeORM/repository/quiz-info-about-user-orm.repository.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/features/quiz/infrastructure/typeORM/repository/quiz-info-about-user-orm.repository.ts
@@ -26,7 +26,7 @@ export class QuizInfoAboutUserOrmRepository {
       .update()
       .set({ score: () => 'score + 1' })
       .where('quizId = :quizId', { quizId })
-      .andWhere('userId = : userId', { userId })
+      .andWhere('userId = :userId', { userId })
       .execute();
 
     return result.affected === 1;
