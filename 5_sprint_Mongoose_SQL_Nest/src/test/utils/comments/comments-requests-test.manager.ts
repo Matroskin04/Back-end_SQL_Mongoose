1d8d33f8c6c0a95e4ee4a4f692b2a99ee36fff4a
Matroskin04 <zetwyn@mail.ru>
2023-10-16 12:21:14 +0700

    added method in comments-request-test-manager 'create9PostsOf3BlogsBy3Users'

--- 5_sprint_Mongoose_SQL_Nest/src/test/utils/comments/comments-requests-test.manager.ts
+++ 5_sprint_Mongoose_SQL_Nest/src/test/utils/comments/comments-requests-test.manager.ts
@@ -14,41 +14,4 @@ export const commentsRequestsTestManager = {
       .set('Authorization', `Bearer ${accessToken}`)
       .query(query);
   },
-  create9PostsOf3BlogsBy3Users: async function (
-    httpServer,
-    blogsId: [string, string, string],
-    accessTokens: [string, string, string],
-    usersId: [string, string, string],
-  ): Promise<PostsAndUsersIdType> {
-    const postNumber = [
-      'first',
-      'second',
-      'third',
-      'fourth',
-      'fifth',
-      'sixth',
-      'seventh',
-      'eighth',
-      'ninth',
-    ];
-    const postsIdsInfo: any = [];
-    let count = 0;
-    for (const i of postNumber) {
-      const result = await this.createPostBlogger(
-        httpServer,
-        blogsId[Math.floor(count / 3)],
-        accessTokens[Math.floor(count / 3)],
-        `Title ${count} ${i}`,
-        `ShortDescription ${count} ${i}`,
-        `Content ${count} ${i}`,
-      );
-      expect(result.statusCode).toBe(HTTP_STATUS_CODE.CREATED_201);
-      postsIdsInfo.push({
-        id: result.body.id,
-        userId: usersId[Math.floor(count / 3)],
-      });
-      count++;
-    }
-    return postsIdsInfo;
-  },
 };
