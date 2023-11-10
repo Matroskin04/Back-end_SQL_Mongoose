3b0589517d05ff58fca357b98ecb08f1ff609d8a
Matroskin04 <zetwyn@mail.ru>
2023-11-10 13:32:16 +0700

    1) added axios 2) created telegram adapter with method for sending webhook to tg

---
+++ src/infrastructure/integrations/telegram.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Post } from '@nestjs/common';
+
+// @Controller('/api/integrations/telegram')
+// export class TelegramController {
+//   constructor() {}
+//
+//   @Post('webhook')
+//   async;
+// }
