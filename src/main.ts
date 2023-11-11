27023c031390325fbf625a2d7fda3f56f13d2fe6
Matroskin04 <zetwyn@mail.ru>
2023-11-11 20:30:59 +0700

    changed getting baseUrl of app, added condition for connecting to ngrok, added some env-s in configuration

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ import process from 'process';
 import { appSettings } from './app.settings';
 import ngrok from 'ngrok';
 import { TelegramAdapter } from './infrastructure/adapters/telegram.adapter';
+import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './configuration/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   appSettings(app);
-
+  const configService: ConfigService<ConfigType> = await app.resolve(
+    ConfigService,
+  );
   const telegramAdapter = await app.resolve(TelegramAdapter);
-  const baseUrl = await ngrok.connect(Number(process.env.PORT) || 5000);
+  let baseUrl = configService.get('app', { infer: true })!.CURRENT_APP_BASE_URL;
+  if (configService.get('app', { infer: true })!.NODE_ENV === 'development') {
+    baseUrl = await ngrok.connect(Number(process.env.PORT) || 5000);
+  }
   await telegramAdapter.sendWebhook(baseUrl);
 
   await app.listen(process.env.PORT || 5000);
