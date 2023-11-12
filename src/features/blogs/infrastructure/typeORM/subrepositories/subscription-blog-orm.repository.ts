a93ace5e335e1184ad000274882e9be284385325
Matroskin04 <zetwyn@mail.ru>
2023-11-12 18:00:49 +0700

    changed logic 'unsubscribing' from deletion on update

--- src/features/blogs/infrastructure/typeORM/subrepositories/subscription-blog-orm.repository.ts
+++ src/features/blogs/infrastructure/typeORM/subrepositories/subscription-blog-orm.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 import { SubscribersOfBlog } from '../../../domain/subscribers-of-blog.entity';
+import { SubscriptionStatusEnum } from '../../../../../infrastructure/utils/enums/blogs-subscribers.enums';
 
 @Injectable()
 export class SubscriptionsBlogOrmRepository {
@@ -15,15 +16,18 @@ export class SubscriptionsBlogOrmRepository {
     const result = await this.subscribersOfBlogRepository.insert({
       blogId,
       userId,
+      subscriptionStatus: SubscriptionStatusEnum.Subscribed,
     });
     return;
   }
 
   async unsubscribeFromBlog(blogId: string, userId: string): Promise<boolean> {
-    const result = await this.subscribersOfBlogRepository.delete({
-      blogId,
-      userId,
-    });
+    const result = await this.subscribersOfBlogRepository.update(
+      { blogId, userId },
+      {
+        subscriptionStatus: SubscriptionStatusEnum.Unsubscribed,
+      },
+    );
     return result.affected === 1;
   }
 }
