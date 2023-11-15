f11435dd3281a0e31f6e273fb32ca3d7d1db433a
Matroskin04 <zetwyn@mail.ru>
2023-11-15 21:32:47 +0700

    changed raw sql repo on typeorm

--- src/infrastructure/guards/forbidden-guards/blog-owner-by-id.guard.ts
+++ src/infrastructure/guards/forbidden-guards/blog-owner-by-id.guard.ts
@@ -3,16 +3,14 @@ import {
   CanActivate,
   ExecutionContext,
   NotFoundException,
-  BadRequestException,
 } from '@nestjs/common';
 import { BlogsQueryRepository } from '../../../features/blogs/infrastructure/SQL/query.repository/blogs.query.repository';
-import { regexpUUID } from '../../utils/regexp/general-regexp';
-import { createBodyErrorBadRequest } from '../../utils/functions/create-error-bad-request.function';
 import { checkIdFormatAndExistence } from '../../utils/functions/check-id-format-and-existence';
+import { BlogsOrmQueryRepository } from '../../../features/blogs/infrastructure/typeORM/query.repository/blogs-orm.query.repository';
 
 @Injectable()
 export class BlogOwnerByIdGuard implements CanActivate {
-  constructor(protected blogsPublicQueryRepository: BlogsQueryRepository) {}
+  constructor(protected blogsPublicQueryRepository: BlogsOrmQueryRepository) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
