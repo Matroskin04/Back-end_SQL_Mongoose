d5c4907315acf9b3e4e03767610245ebfebc6926
Matroskin04 <zetwyn@mail.ru>
2023-12-07 11:07:56 +0700

    added description example for registration api swagger

--- src/features/auth/api/models/input/login.input.model.ts
+++ src/features/auth/api/models/input/login.input.model.ts
@@ -4,11 +4,14 @@ import { ApiProperty } from '@nestjs/swagger';
 export class LoginInputModel {
   @IsNotEmpty({ message: 'The field shouldn\t be empty' })
   @IsString({ message: 'It should be a string' })
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Email / login of a user',
+    example: 'somemail@gmail.com / login123',
+  })
   loginOrEmail: string;
 
   @IsNotEmpty({ message: 'The field shouldn\t be empty' })
   @IsString({ message: 'It should be a string' })
-  @ApiProperty()
+  @ApiProperty({ description: 'Password of a user', example: 'qwerty' })
   password: string;
 }
